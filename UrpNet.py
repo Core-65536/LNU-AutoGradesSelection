87b5a1c4a860a9de72982b7c5510460518c0bca7
Core-65536 <73655341+Core-65536@users.noreply.github.com>
2024-07-24 15:51:32 +0800

    Update UrpNet.py

--- UrpNet.py
+++ UrpNet.py
@@ -1,13 +1,17 @@
 import requests
 
-# Author : Core_65536
-# Version : 1.0
-# Copyright (c) 2024 Core_65536. All rights reserved.
-
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                  'Chrome/126.0.0.0 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 '
+                  'Safari/537.36',
+    'Accept': '*/*',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'Accept-Encoding': 'gzip, deflate',
+    'Referer': 'http://jwstudent.lnu.edu.cn/login',
 }
+
+
+def Set_headers(new_headers):
+    headers['Referer'] = new_headers
 
 
 # 任何一个处理成功抛出错误
@@ -16,11 +20,11 @@ def Loop_GET(session, url, data):
     rp = None
     while flag == 0:
         try:
-            rp = session.get(url, data=data, headers=headers, timeout=10)
+            rp = session.get(url, data=data, headers=headers, timeout=5)
             flag = 1
         except requests.exceptions.RequestException as e:
             flag = 0
-            print('Retrying...')
+            print('Timeout,Retrying...')
     return rp
 
 
@@ -33,5 +37,5 @@ def Loop_POST(session, url, data):
             flag = 1
         except requests.exceptions.RequestException as e:
             flag = 0
-            print('Retrying...')
+            print('Timeout,Retrying...')
     return rp
